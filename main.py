c80e38e20318b70dfd1fd0e5ef3003f65cdc1a85
rokisto2 <gaponenkooleg0@gmail.com>
2023-06-04 18:14:35 +0300

    add main window

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from start_window import Ui_MainWindow
-
+from text_resource import  labelINfo
 
 class ExpenseTracker(QMainWindow):
     def __init__(self):
         super(ExpenseTracker, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.self_UI()
 
+    def self_UI(self):
+        self.ui.label.setText("Просто текстовая аннотация")
+        self.ui.label_2.setText(labelINfo.info1)
+        self.ui.pushButton.clicked.connect(self.pussh_start_button)
+
+    def pussh_start_button(self):
+        print(1)
+        app.exit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- start_window.py
+++ start_window.py
@@ -17,16 +17,20 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
     QSizePolicy, QWidget)
+import res_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(737, 509)
-        MainWindow.setStyleSheet(u"\n"
+        MainWindow.setEnabled(True)
+        MainWindow.resize(1300, 916)
+        MainWindow.setContextMenuPolicy(Qt.NoContextMenu)
+        MainWindow.setStyleSheet(u"/* Frame 1 */\n"
+"\n"
 "position: relative;\n"
-"width: 737px;\n"
-"height: 484px;\n"
+"width: 1314px;\n"
+"height: 916px;\n"
 "\n"
 "background: #000000;\n"
 "\n"
@@ -34,101 +38,101 @@ class Ui_MainWindow(object):
 "")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(341, 97, 631, 68))
+        self.label.setStyleSheet(u"/* \u041f\u0440\u043e\u0441\u0442\u043e \u0442\u0435\u043a\u0441\u0442\u043e\u0432\u0430\u044f \u0430\u043d\u043d\u043e\u0442\u0430\u0446\u0438\u044f */\n"
+"\n"
+"background-color: rgba(0, 0, 0, 0);\n"
+"\n"
+"position: absolute;\n"
+"width: 631px;\n"
+"height: 68px;\n"
+"left: 341px;\n"
+"top: 97px;\n"
+"\n"
+"font-family: 'Inter';\n"
+"font-style: normal;\n"
+"font-weight: 400;\n"
+"font-size: 44px;\n"
+"line-height: 53px;\n"
+"\n"
+"color: #FFFFFF;\n"
+"\n"
+"")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(82, 185, 1150, 566))
+        self.label_2.setStyleSheet(u"/* \u041a\u0430\u043a \u0432 \u043b\u0438\u0446\u0435\u043d\u0437\u0438\u043e\u043d\u043d\u043e\u043c \u0441\u043e\u0433\u043b\u0430\u0448\u0435\u043d\u0438\u0438. */\n"
+"background-color: rgba(255, 255, 255, 0);\n"
+"position: absolute;\n"
+"width: 1150px;\n"
+"height: 566px;\n"
+"left: 82px;\n"
+"top: 183px;\n"
+"\n"
+"font-family: 'Inter';\n"
+"font-style: normal;\n"
+"font-weight: 400;\n"
+"font-size: 29px;\n"
+"line-height: 35px;\n"
+"\n"
+"color: #FFFFFF;\n"
+"\n"
+"")
+        self.label_2.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(171, 431, 395, 54))
-        self.pushButton.setStyleSheet(u"/* button */\n"
+        self.pushButton.setGeometry(QRect(307, 767, 700, 98))
+        self.pushButton.setStyleSheet(u"/* Button */\n"
 "\n"
 "position: absolute;\n"
-"width: 395px;\n"
-"height: 54px;\n"
-"left: 171px;\n"
-"top: 431px;\n"
+"width: 700px;\n"
+"height: 98px;\n"
+"left: 307px;\n"
+"top: 767px;\n"
 "\n"
 "\n"
 "\n"
-"/* Rectangle 3 */\n"
+"/* Rectangle 1 */\n"
 "\n"
 "position: absolute;\n"
-"width: 395px;\n"
-"height: 52px;\n"
-"left: 171px;\n"
-"top: 433px;\n"
+"width: 700px;\n"
+"height: 98px;\n"
+"left: 307px;\n"
+"top: 767px;\n"
 "\n"
-"background: #24BAFF;\n"
-"border-radius: 8px;\n"
+"background: #009158;\n"
+"border-radius: 13px;\n"
 "\n"
 "\n"
 "/* \u041f\u0440\u0438\u0441\u0442\u0443\u043f\u0438\u0442\u044c! */\n"
 "\n"
 "position: absolute;\n"
-"width: 392px;\n"
-"height: 53px;\n"
-"left: 174px;\n"
-"top: 431px;\n"
-"\n"
-"font-family: 'Inder';\n"
-"font-style: normal;\n"
-"font-weight: 400;\n"
-"font-size: 24px;\n"
-"line-height: 51px;\n"
-"/* or 212% */\n"
-"text-align: center;\n"
-"letter-spacing: 0.02em;\n"
-"\n"
-"color: #FFFFFF;\n"
-"\n"
-"")
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(195, 56, 348, 28))
-        self.label.setStyleSheet(u"/* \u041f\u0440\u043e\u0441\u0442\u043e \u0442\u0435\u043a\u0441\u0442\u043e\u0432\u0430\u044f \u0430\u043d\u043d\u043e\u0442\u0430\u0446\u0438\u044f */\n"
-"\n"
-"position: absolute;\n"
-"width: 348px;\n"
-"height: 62px;\n"
-"left: 195px;\n"
-"top: 56px;\n"
+"width: 353px;\n"
+"height: 46px;\n"
+"left: 524px;\n"
+"top: 791px;\n"
 "\n"
 "font-family: 'Inter';\n"
 "font-style: normal;\n"
 "font-weight: 400;\n"
-"font-size: 24px;\n"
-"line-height: 29px;\n"
-"/* or 121% */\n"
-"text-align: center;\n"
-"letter-spacing: 0.01em;\n"
+"font-size: 44px;\n"
+"line-height: 53px;\n"
 "\n"
 "color: #FFFFFF;\n"
 "\n"
 "")
-        self.label.setScaledContents(False)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(76, 114, 622, 305))
-        self.label_2.setStyleSheet(u"/* \u041a\u0430\u043a \u0432 \u043b\u0438\u0446\u0438\u043d\u0437\u0438\u043e\u043d\u043d\u043e\u043c \u0441\u043e\u0433\u043b\u0430\u0448\u0435\u043d\u0438\u0438\u0438 */\n"
-"\n"
-"position: absolute;\n"
-"width: 622px;\n"
-"height: 305px;\n"
-"left: 76px;\n"
-"top: 114px;\n"
-"\n"
-"font-family: 'Italianno';\n"
-"font-style: normal;\n"
-"font-weight: 400;\n"
-"font-size: 17px;\n"
-"line-height: 4px;\n"
-"/* or 24% */\n"
-"letter-spacing: 0.01em;\n"
-"\n"
-"color: #FFFFFF;\n"
-"\n"
-"opacity: 0.76;\n"
-"")
-        self.label_2.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(0, 0, 1301, 916))
+        self.label_3.setPixmap(QPixmap(u":/fon/resurce/1.png"))
         MainWindow.setCentralWidget(self.centralwidget)
+        self.label_3.raise_()
+        self.label.raise_()
+        self.label_2.raise_()
+        self.pushButton.raise_()
 
         self.retranslateUi(MainWindow)
 
@@ -137,9 +141,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u041f\u0440\u043e\u0441\u0442\u043e \u0442\u0435\u043a\u0441\u0442\u043e\u0432\u0430\u044f \u0430\u043d\u043d\u043e\u0442\u0430\u0446\u0438\u044f", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u041a\u0430\u043a \u0432 \u043b\u0438\u0446\u0435\u043d\u0437\u0438\u043e\u043d\u043d\u043e\u043c \u0441\u043e\u0433\u043b\u0430\u0448\u0435\u043d\u0438\u0438.", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u041f\u0440\u0438\u0441\u0442\u0443\u043f\u0438\u0442\u044c!", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"\u041f\u0440\u043e\u0441\u0442\u043e \u0442\u0435\u043a\u0441\u0442\u043e\u0432\u0430\u044f \u0430\u043d\u043d\u043e\u0442\u0430\u0446\u0438\u044f\n"
-"", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u041a\u0430\u043a \u0432 \u043b\u0438\u0446\u0438\u043d\u0437\u0438\u043e\u043d\u043d\u043e\u043c \u0441\u043e\u0433\u043b\u0430\u0448\u0435\u043d\u0438\u0438\u0438", None))
+        self.label_3.setText("")
     # retranslateUi
 
